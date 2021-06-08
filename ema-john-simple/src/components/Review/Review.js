aeee5d1c5b1fa4c37a2bf013d652af20faa59617
niazmurshedsiam <niazmurshed16@gmail.com>
2021-06-09 01:42:20 +0600

    Remove from cart, pass parameter on click, remove from state

--- ema-john-simple/src/components/Review/Review.js
+++ ema-john-simple/src/components/Review/Review.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
@@ -16,11 +16,18 @@ const Review = () => {
        setCart(cartProducts);
 
     },[])
+    const removeProduct = (productKey)=>{
+        console.log("Remove Product",productKey);
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+
+    }
     return (
         <div>
-            <h1>This is Review {cart.length}</h1>
+            {/* <h1>This is Review {cart.length}</h1> */}
             {
-                cart.map(product =><ReviewItem product={product} key={product.key}></ReviewItem>)
+                cart.map(product =><ReviewItem product={product} removeProduct={removeProduct} key={product.key}></ReviewItem>)
             }
         </div>
     );
--- ema-john-simple/src/components/ReviewItem/ReviewItem.js
+++ ema-john-simple/src/components/ReviewItem/ReviewItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ReviewItem = (props) => {
-    const {name, quantity} = props.product;
+    const {name, quantity,key} = props.product;
     const reviewItemStyle={
         borderBottom:'1px solid lightgray',
         marginBottom:'5px',
@@ -13,7 +13,7 @@ const ReviewItem = (props) => {
         <div style={reviewItemStyle} className="review-item">
             <h3 style={{color:'blue'}} className="product-details">{name}</h3>
             <p>Quantity: {quantity}</p>
-            <button className="main-button">Remove</button>
+            <button className="main-button" onClick={()=>props.removeProduct(key)}>Remove</button>
         </div>
     );
 };
