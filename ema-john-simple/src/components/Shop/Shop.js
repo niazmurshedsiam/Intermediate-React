63bd213822ad07f263e5e590f40133d05b8a2bb5
niazmurshedsiam <niazmurshed16@gmail.com>
2021-06-15 15:57:17 +0600

    React Developer Tool, Chrome extension, Refactor add to Cart

--- ema-john-simple/src/components/Shop/Shop.js
+++ ema-john-simple/src/components/Shop/Shop.js
@@ -10,11 +10,24 @@ const Shop = () => {
   const [products, setProducts] = useState(first10);
   const [cart,setCart] = useState([]);
   const handleAddProduct = (product)=>{
-      console.log('cliked',product);
-      const newCart = [...cart,product];
+      // console.log('cliked',product);
+      const toBeAddedKey = product.key;
+      const sameProduct = cart.find(pd=>pd.key===product.key);
+      let count = 1;
+      let newCart;
+      if(sameProduct){
+        count = sameProduct.quantity + 1;
+        sameProduct.quantity = sameProduct.quantity + count;
+        const others = cart.filter(pd => pd.key !== toBeAddedKey);
+        newCart = [...others,sameProduct];
+      }
+      else{
+        product.quantity=1;
+        newCart=[...cart,product];
+      }
+      
       setCart(newCart);
-      const sameProduct = newCart.filter(pd=>pd.key===product.key);
-      const count = sameProduct.length;
+      
       addToDatabaseCart(product.key,count);
   }
   // console.log(fakeData);
