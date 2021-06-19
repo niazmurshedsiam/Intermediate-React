aa66a065ad1ea2d86cd150bf439e46a049647e4d
niazmurshedsiam <niazmurshed16@gmail.com>
2021-06-19 20:39:03 +0600

    work start context api

---
+++ my-scss-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import Shipment from './component/Shipment/Shipment';
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+      <Shipment></Shipment>
+    </div>
+  );
+}
+
+export default App;
